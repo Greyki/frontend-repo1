cb7dac36fa703ed2b338d8a0052f55686e8cef1d
Guido Cortes Leyton <guido.cortes.l@hotmail.com>
2026-06-22 15:40:53 -0400

    Prueba de CRUD con LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import ReactDOM from 'react-dom';
+import Form from './components/Form';
+import List from './components/List';
 
 function App() {
 
-  const [cuenta, setCuenta] = useState(0)
-  const contar = () => setCuenta(cuenta + 1)
+  const [items, setItems] = useState(() => {
+    return JSON.parse(localStorage.getItem('items')) || [];
+  });
+  const [itemToEdit, setItemToEdit] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
+
+  const addOrUpdateItem = (value) => {
+    if (itemToEdit) {
+      setItems(items.map(item => item.id === itemToEdit.id ? {...item,value} : item));
+      setItemToEdit(null);
+    } else {
+      setItems([...items,{id: Date.now(),value}]);
+    }
+  };
+
+  const deleteItem = (id) => {
+    setItems(items.filter(item => item.id !== id  ));
+  };
+
+  const editItem = (item) => {
+    setItemToEdit(item);
+  }
 
   return (
-    <>
-      <div className='container mt-3'>
-        <button onClick={contar}>Clickea Aquí</button>
-        <p>Clicks {cuenta}</p>
-      </div>
-    </>
+    <div className='App container mb-3'>
+      <h1>CRUD LocalStorage</h1>
+      <Form
+        addOrUpdateItem={addOrUpdateItem}
+        itemToEdit={itemToEdit}/>
+      <List items={items}
+        deleteItem={deleteItem} editItem={editItem}/>
+    </div>
   );
 }
 
